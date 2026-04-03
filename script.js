9a68a99eeee898b9f0f6a8c6af25fbbd65a3e31c
Kevin Reyes <kevinreyesgeo@gmail.com>
2026-04-03 17:03:03 -0600

    portafolio inicial

---
+++ script.js
@@ -0,0 +1,21 @@
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 80 },
+        color: { value: "#00ffff" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        move: {
+            enable: true,
+            speed: 1
+        }
+    },
+    interactivity: {
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            }
+        }
+    }
+});
