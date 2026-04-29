7d6c8ecae438dc060334d4aca4bf7f87d3e62612
KevinReyes21 <kevinreyesgeo@gmail.com>
2026-04-29 16:58:47 -0600

    Update: mapa trabajos

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let puntoActivo = null;
+
+function getColor(tipo) {
+  switch(tipo) {
+    case "gnss": return "#00f0ff";          // cyan neón
+    case "dron": return "#ff00ff";          // magenta
+    case "gnss_dron": return "#9d00ff";     // violeta
+    case "estacion_total": return "#00ff85"; // verde neón
+    case "procesamiento": return "#ffae00"; // naranja brillante
+    default: return "#ffffff";
+  }
+}
+
 particlesJS("particles-js", {
     particles: {
         number: { value: 80 },
@@ -19,4 +32,86 @@ particlesJS("particles-js", {
         }
     }
 });
+
+// MAPA TRABAJOS DE CAMPO
+var map = L.map('map').setView([23.5, -102], 5);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; OpenStreetMap &copy; CartoDB'
+}).addTo(map);
+
+fetch('data/trabajos.geojson')
+  .then(res => res.json())
+  .then(data => {
+
+            L.geoJSON(data, {
+
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+            radius: 6,
+            fillColor: getColor(feature.properties.instrumento),
+            color: "#000",
+            weight: 1,
+            fillOpacity: 0.9,
+            className: 'glow-marker',
+            });
+        },
+
+        onEachFeature: function (feature, layer) {
+
+        layer.on({
+            click: function (e) {
+
+            let coords = e.latlng;
+            let currentZoom = map.getZoom();
+
+            let targetZoom = currentZoom < 13 ? 13 : currentZoom;
+
+            map.flyTo(coords, targetZoom, {
+            duration: 0.1
+            });
+
+
+            actualizarPanel(feature.properties);
+            },
+
+            mouseover: function (e) {
+            let layer = e.target;
+
+            layer.setStyle({
+                radius: 10,
+                weight: 3,
+                fillOpacity: 1
+            });
+            },
+
+            mouseout: function (e) {
+            let layer = e.target;
+
+            layer.setStyle({
+                radius: 7,
+                weight: 2,
+                fillOpacity: 0.9
+            });
+            }
+        });
+
+        }
+
+        }).addTo(map);
+
+        
+
+  });
+
+function actualizarPanel(props) {
+
+  document.getElementById("info-panel").innerHTML = `
+    <h3>${props.trabajo}</h3>
+
+    <p><strong>Instrumento:</strong> ${props.instrumento}</p>
+
+    <p>${props.descripcion}</p>
+  `;
+}
 
